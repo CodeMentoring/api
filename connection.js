c7b6bf0accbf37e3423d73af160636de4325f485
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2023-11-08 20:25:37 -0500

    Add files via upload

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require("sequelize");
 
-const database = "library";
-const username = "postgres";
-const password = "adminadmin";
-const host = "localhost";
-const port = 5434;
+const database = "library_db_0t1y"; //"library";
+const username = "hamp"; //"postgres";
+const password = "JaQgOSs1o6R1OXxex4YeEfNFOHx8ZKOF"; //"adminadmin";
+const host = "dpg-cl638u2l7jac73d521vg-a"; //"localhost";
+const port = 5432; //5434;
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
